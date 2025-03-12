d0ef1687ca0d3e643388f6275cba74df2997209f
jeel11p <jeel11patel@gmail.com>
2025-03-11 17:45:22 -0700

    Implemented Settings Menu

--- Assets/_Scripts/InputManager.cs
+++ Assets/_Scripts/InputManager.cs
@@ -6,6 +6,7 @@ public class InputManager : MonoBehaviour
     public UnityEvent<Vector2> OnMove = new();
     public UnityEvent OnJump = new();
     public UnityEvent OnDash = new();
+    public UnityEvent OnSettingsMenu = new();
     void Update()
     {
         Vector2 input = Vector2.zero;
@@ -30,10 +31,16 @@ public class InputManager : MonoBehaviour
             OnJump?.Invoke();
         }
 
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            OnSettingsMenu?.Invoke();
+        }
+
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             OnDash?.Invoke();
         }
         OnMove?.Invoke(input.normalized);
+        if (GameManager.Instance.IsSettingsMenuActive) return;
     }
 }
